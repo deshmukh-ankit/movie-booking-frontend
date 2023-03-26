c191b7e9da2fede668b9988b3e473c5f0654ff29
Ankit Deshmukh <deshmukhankit678@gmail.com>
2023-03-26 11:31:26 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import axios from 'axios';
+import {Provider} from 'react-redux'
+import {BrowserRouter} from 'react-router-dom';
+import { store } from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//axios.defaults.baseURL="http://localhost:4500";
+axios.defaults.baseURL="https://movie-booking-backend-pqjg.onrender.com";
 root.render(
   <React.StrictMode>
+  <BrowserRouter>
+  <Provider store={store}>
     <App />
+    </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
